11569304601dd1f5aacf00fd2fc63af06c7f4d2b
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2022-01-22 14:12:57 +0530

    Add curve and update nav bar

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ function closeMenu(){
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+var lastScrollTop = 0;
+var nav = document.querySelector(".navbar");
+window.addEventListener("scroll", ()=>{
+var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        if (top > 40 && lastScrollTop-top<=0){
+            nav.style.opacity="0";
+            nav.style.transform="translateY(-50px)"
+	}
+	if(lastScrollTop-top>5){
+	    nav.style.opacity="1";
+        nav.style.transform="";
+	}
+	lastScrollTop=top;
+});
